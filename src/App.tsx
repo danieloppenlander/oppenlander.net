36247a96eaad1efc25b29c432c7745abac154513
Daniel Oppenlander <daniel@oppenlander.net>
2022-01-25 16:07:29 -0800

    fixed linting?

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import About from './components/About';
+import React from "react";
+import "./App.css";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
-        <About/>
+      <About />
     </div>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function About() {
-    return (
-        <div className="About">
-            hello, world
-        </div>
-    );
+  return <div className="About">hello, world</div>;
 }
 
 export default About;
