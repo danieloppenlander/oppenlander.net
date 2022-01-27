fd8caa28697f229d9767e862a80ffca58fb8c283
Daniel Oppenlander <daniel@oppenlander.net>
2022-01-26 17:43:12 -0800

    fixed build

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.css";
 
 import SplashHeading from "./components/SplashHeading";
-import Content from "./components/Content";
-import { Col, Container, Row } from "react-grid-system";
 
 function App() {
   return (
