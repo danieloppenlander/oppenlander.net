8ada0076b6746403b087a39cd34376dad4383949
danieloppenlander <danieloppenlander@users.noreply.github.com>
2022-01-27 01:40:10 +0000

    Prettified Code!

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import "./App.css";
 
 import SplashHeading from "./components/SplashHeading";
 import Content from "./components/Content";
-import {Col, Container, Row} from "react-grid-system";
+import { Col, Container, Row } from "react-grid-system";
 
 function App() {
   return (
-      <div className={"App"}>
-        <section className={"skew-top"}>
-          <SplashHeading />
-        </section>
-        <section className={"skew-bottom"}>
-          {/*<Content />*/}
-        </section>
-      </div>
+    <div className={"App"}>
+      <section className={"skew-top"}>
+        <SplashHeading />
+      </section>
+      <section className={"skew-bottom"}>{/*<Content />*/}</section>
+    </div>
   );
 }
 
--- src/components/SplashHeading.tsx
+++ src/components/SplashHeading.tsx
@@ -5,34 +5,31 @@ import { Container, Row, Col } from "react-grid-system";
 
 function SplashHeading() {
   return (
-      <Container className={"SplashHeading centered"}>
-        <Row>
-          <Col md={6} className={"heading"}>
-            <h1>Daniel Oppenlander</h1>
-            <p>
-              An engineer with a career approach that includes constant learning
-              and personal integrity.
-            </p>
-          </Col>
-          <Col md={3} offset={{md:3}} className={"buttons"}>
-            <a href={"/daniel-oppenlander-resume.pdf"} className={"button-a"}>
-              <div className={"button"}>
-                Resume
-              </div>
-            </a>
-            <a href={"mailto: daniel@oppenlander.net"} className={"button-a"}>
-              <div className={"button"}>
-                Email
-              </div>
-            </a>
-            <a href={"https://www.linkedin.com/in/daniel-oppenlander/"} className={"button-a"}>
-              <div className={"button"}>
-                LinkedIn
-              </div>
-            </a>
-          </Col>
-        </Row>
-      </Container>
+    <Container className={"SplashHeading centered"}>
+      <Row>
+        <Col md={6} className={"heading"}>
+          <h1>Daniel Oppenlander</h1>
+          <p>
+            An engineer with a career approach that includes constant learning
+            and personal integrity.
+          </p>
+        </Col>
+        <Col md={3} offset={{ md: 3 }} className={"buttons"}>
+          <a href={"/daniel-oppenlander-resume.pdf"} className={"button-a"}>
+            <div className={"button"}>Resume</div>
+          </a>
+          <a href={"mailto: daniel@oppenlander.net"} className={"button-a"}>
+            <div className={"button"}>Email</div>
+          </a>
+          <a
+            href={"https://www.linkedin.com/in/daniel-oppenlander/"}
+            className={"button-a"}
+          >
+            <div className={"button"}>LinkedIn</div>
+          </a>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
