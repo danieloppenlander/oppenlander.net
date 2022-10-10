494a7000cd8a03cb20520863c54d15d83b9d7af3
Daniel Oppenlander <daniel@oppenlander.net>
2022-10-10 09:15:23 -0700

    added headshot, updated styling

--- src/components/SplashHeading.tsx
+++ src/components/SplashHeading.tsx
@@ -7,7 +7,12 @@ function SplashHeading() {
   return (
     <Container className={"SplashHeading"}>
       <Row>
-        <Col md={12} lg={6}>
+        <Col md={12}>
+          <img id={"headshot"} src={"/img/avatar-pro.jpg"} alt={"Headshot"}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={8} lg={6} offset={{sm: 0, md: 2, lg: 3}}>
           <h1>Daniel Oppenlander</h1>
           <p className={"text-italic"}>
             An engineer with a career approach that includes{" "}
@@ -15,45 +20,45 @@ function SplashHeading() {
             <span className={"text-blue"}>personal integrity</span>.
           </p>
         </Col>
-        <Col md={12} lg={6}>
-          <Row>
-            <Col xs={6} lg={12} offset={{ lg: 6 }}>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={"/files/daniel-oppenlander-resume.pdf"}
-                className={"button gradient-blue gradient-blue-hover"}
-              >
-                Resume
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={"mailto: daniel@oppenlander.net"}
-                className={"button gradient-blue gradient-blue-hover"}
-              >
-                Email
-              </a>
-            </Col>
-            <Col xs={6} lg={12} offset={{ lg: 6 }}>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={"https://www.linkedin.com/in/daniel-oppenlander/"}
-                className={"button gradient-orange gradient-orange-hover"}
-              >
-                LinkedIn
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={"https://github.com/danieloppenlander"}
-                className={"button gradient-orange gradient-orange-hover"}
-              >
-                GitHub
-              </a>
-            </Col>
-          </Row>
+      </Row>
+      <Row>
+        <Col md={6} xl={4} offset={{md: 3, xl: 4}}>
+          <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={"/files/daniel-oppenlander-resume.pdf"}
+              className={"button float-left gradient-blue gradient-blue-hover"}
+          >
+            Resume
+          </a>
+          <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={"https://www.linkedin.com/in/daniel-oppenlander/"}
+              className={"button float-right gradient-orange gradient-orange-hover"}
+          >
+            LinkedIn
+          </a>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} xl={4} offset={{md: 3, xl: 4}}>
+          <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={"mailto: daniel@oppenlander.net"}
+              className={"button float-left gradient-blue gradient-blue-hover"}
+          >
+            Email
+          </a>
+          <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={"https://github.com/danieloppenlander"}
+              className={"button float-right gradient-orange gradient-orange-hover"}
+          >
+            GitHub
+          </a>
         </Col>
       </Row>
     </Container>
