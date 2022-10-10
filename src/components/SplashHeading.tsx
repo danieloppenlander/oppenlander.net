38dec213b89b376ad91f38ba22868a72b214fb74
danieloppenlander <danieloppenlander@users.noreply.github.com>
2022-10-10 16:42:47 +0000

    Prettified Code!

--- src/components/SplashHeading.tsx
+++ src/components/SplashHeading.tsx
@@ -8,11 +8,11 @@ function SplashHeading() {
     <Container className={"SplashHeading"}>
       <Row>
         <Col md={12}>
-          <img id={"headshot"} src={"/img/avatar-pro.jpg"} alt={"Headshot"}/>
+          <img id={"headshot"} src={"/img/avatar-pro.jpg"} alt={"Headshot"} />
         </Col>
       </Row>
       <Row>
-        <Col md={7} xl={5} offset={{md: 3, xl: 4}}>
+        <Col md={7} xl={5} offset={{ md: 3, xl: 4 }}>
           <h1 id={"name"}>Daniel Oppenlander</h1>
           <p className={"text-italic"}>
             An engineer with a career approach that includes{" "}
@@ -22,40 +22,44 @@ function SplashHeading() {
         </Col>
       </Row>
       <Row>
-        <Col md={6} xl={4} offset={{md: 3, xl: 4}}>
+        <Col md={6} xl={4} offset={{ md: 3, xl: 4 }}>
           <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={"/files/daniel-oppenlander-resume.pdf"}
-              className={"button float-left gradient-blue gradient-blue-hover"}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={"/files/daniel-oppenlander-resume.pdf"}
+            className={"button float-left gradient-blue gradient-blue-hover"}
           >
             Resume
           </a>
           <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={"https://www.linkedin.com/in/daniel-oppenlander/"}
-              className={"button float-right gradient-orange gradient-orange-hover"}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={"https://www.linkedin.com/in/daniel-oppenlander/"}
+            className={
+              "button float-right gradient-orange gradient-orange-hover"
+            }
           >
             LinkedIn
           </a>
         </Col>
       </Row>
       <Row>
-        <Col md={6} xl={4} offset={{md: 3, xl: 4}}>
+        <Col md={6} xl={4} offset={{ md: 3, xl: 4 }}>
           <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={"mailto: daniel@oppenlander.net"}
-              className={"button float-left gradient-blue gradient-blue-hover"}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={"mailto: daniel@oppenlander.net"}
+            className={"button float-left gradient-blue gradient-blue-hover"}
           >
             Email
           </a>
           <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={"https://github.com/danieloppenlander"}
-              className={"button float-right gradient-orange gradient-orange-hover"}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={"https://github.com/danieloppenlander"}
+            className={
+              "button float-right gradient-orange gradient-orange-hover"
+            }
           >
             GitHub
           </a>
