b070feda083afbd61f4fd71c7dc6f6ccb46e7711
Daniel Oppenlander <daniel@oppenlander.net>
2025-08-27 12:58:46 -0700

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,12 @@ function App() {
       <section className={"skew-top"}>
         <SplashHeading />
       </section>
-      <svg className={"skew"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <svg
+        className={"skew"}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 10 10"
+        preserveAspectRatio="none"
+      >
         <polygon points="0,0 10,10 0,10" />
       </svg>
       <section className={"skew-bottom"}></section>
